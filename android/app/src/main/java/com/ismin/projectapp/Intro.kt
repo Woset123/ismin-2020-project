c326383805cd71128cce8b12c72b1ba7234e575b
Woset123 <ericdall@orange.fr>
2020-11-28 15:17:53 +0100

    Add Rotation in Splash Screen

--- android/app/src/main/java/com/ismin/projectapp/Intro.kt
+++ android/app/src/main/java/com/ismin/projectapp/Intro.kt
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class Intro : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
         supportActionBar?.hide()
+
+        val rotate = this.findViewById<ImageView>(R.id.rot)
+        rotate.startAnimation(
+            AnimationUtils.loadAnimation(this, R.anim.rotate)
+        )
 
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
